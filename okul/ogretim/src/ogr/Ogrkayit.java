5e13a1086fbab6cd665ef14fbd5155d14e454bdb
Egemen Öztunçel <64845652+ditto12@users.noreply.github.com>
2020-05-31 16:43:11 +0300

    egemen öztunçel burakhan küskü final

---
+++ okul/ogretim/src/ogr/Ogrkayit.java
@@ -0,0 +1,64 @@
+package ogr;
+
+import java.lang.reflect.Field;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import ogr.Ogretmen;
+
+
+public class Ogrkayit {
+	
+	public int ogrkayit(Ogretmen ogr) throws SQLException
+	{	 
+		/* veri tabaný baðlantý kodu */
+		String connUrl= "jdbc:sqlserver://localhost:1433;databaseName=okuldb;integratedSecurity=true;";
+		Connection conn = DriverManager.getConnection(connUrl);
+		Statement stmt = conn.createStatement();
+		
+		try
+		{   
+			/* kayýt ekleme sql sorgusu */			
+			PreparedStatement pst = conn.prepareStatement("INSERT INTO ogretmen_kayit values (?,?,?,?)");
+			/* formdan alýnan verileri veri tabanýna atma */
+			pst.setString(1, ogr.getUnvan());
+			pst.setString(2, ogr.getAd());
+			pst.setString(3, ogr.getSoyad());
+			pst.setInt(4, ogr.getDers());	
+			
+			/* ders id ile öðretim görevlisinin seçtiði ders birbirine eþitse bunlarý alýr */
+			ResultSet rs = stmt.executeQuery("SELECT dolu FROM dersler WHERE ders_id = "+ogr.getDers()+"");
+			while (rs.next ()) {  
+     		int dks = rs.getInt ("dolu");
+			System.out.println ("dolu:" + dks );
+			    /* eðer dolu 1 ise dersin dolu olduðunu gösterir */
+				if(dks == 1)
+				{
+					System.out.println("dolu ders");
+					return 0;
+				}
+				
+			}			
+			pst.execute();
+				
+			/* ders id ile öðretim görevlisinin seçtiði ders birbirine eþitse dolu'yu 1 yapar (dersin alýndýðýný gösterir) */		
+			PreparedStatement pst2 = conn.prepareStatement("UPDATE dersler set dolu = 1 where ders_id = "+ ogr.getDers() +" ");			
+			pst2.execute();
+			
+		}
+		
+		catch (Exception e) {
+	    System.out.println(e);
+	    
+		}
+           return 1;
+	}
+
+}
